cc30f093ef3c57667fd5c0b3ac2d6dbd63680de2
li.ssus <132607100+lissus01@users.noreply.github.com>
2025-07-07 17:33:36 -0400

    Add files via upload

--- src/app/models/cita.model.ts
+++ src/app/models/cita.model.ts
@@ -1,32 +1,39 @@
+// cita.model.ts
+
 export interface Cita {
   id?: string;
   veterinarioId: string;
   veterinarioNombre: string;
-  fecha: Date;
+  fecha: string; // Formato 'YYYY-MM-DD'
   hora: string;
   motivo: string;
-  petId: string; // Opcional
-  petName?: string; // Opcional
-  estado: 'pendiente' | 'confirmada' | 'cancelada' | 'completada';
+  petId: string;
+  petName?: string;
+  estado: 'pendiente' | 'confirmada' | 'cancelada' | 'completada'; // Actualizado para incluir 'cancelada'
   userId: string;
   fechaCreacion: Date;
-  recetaBase64?: string; // Cambiamos a base64 string
+  recetaBase64?: string;
   vetName?: string;
-  motivoCancelacion?: string;
+  motivoCancelacion?: string; // ¡Nuevo! Campo para el motivo de la cancelación
 }
 
 // Tipo para el estado de la cita
 export type EstadoCita = 'pendiente' | 'confirmada' | 'cancelada' | 'completada';
 
-// Creamos una función para inicializar una nueva cita
+// Función para inicializar una nueva cita
 export function nuevaCita(userId: string = '', petId: string= ''): Cita {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Los meses son 0-indexados, por eso se le suma 1
+  const day = today.getDate().toString().padStart(2, '0');
+
   return {
     veterinarioId: '',
     veterinarioNombre: '',
-    fecha: new Date(),
+    fecha: `${year}-${month}-${day}`, // Formato 'YYYY-MM-DD'
     hora: '',
     motivo: '',
-    estado: 'pendiente',
+    estado: 'pendiente', // Por defecto, una cita nueva siempre estará 'pendiente'
     userId: userId,
     petId: petId,
     fechaCreacion: new Date()
